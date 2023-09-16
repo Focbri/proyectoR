4c89756dfb171edf0e5c49a891fcd8e8c54d883e
Joan <joanfpg2002@gmail.com>
2023-09-16 16:29:50 -0500

    Se Agrega el proyecto

---
+++ Views/V_Inicio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="/Ripley/Views/style/S_Inicio.css">
+  <link rel="icon" href="/Ripley/img/logo.webp">
+  
+  <title>Ripley</title>
+</head>
+<body>
+  <header>
+    <a href="https://simple.ripley.com.pe/minisitios/especial/app-ripley-peru" target="_blank"><img src="/Ripley/img/ripleyapp.webp" class="ripleyapp"></a>
+    <nav></nav>
+  </header>
+  <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni et nobis eum doloremque aspernatur? Asperiores aperiam voluptatem aut illum quasi. Nulla at eveniet perspiciatis quos. At impedit repellendus velit fuga. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis porro ratione culpa. Iure, voluptate ipsam vitae accusamus distinctio et vel, aliquam veniam recusandae ducimus, tempore facere maxime natus iusto excepturi.</p>
+</body>
+</html>
